79684194d6f3a60496426a69f02b30fd83bd058d
yangzhiyun <yangzhiyunppp@outlook.com>
2026-02-15 00:24:51 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -33,4 +33,11 @@ public interface EmployeeMapper {
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 根据id查询员工信息
+     * @param employee
+     * @return
+     */
+    void update(Employee employee);
 }
